bd28207b7a0cf937c2f11756d4ed3c7e6faa9d01
ANTi1302 <porkoe3443334@gmail.com>
2022-01-19 08:27:24 +0700

    data

---
+++ jsondata/import.bat
@@ -0,0 +1,4 @@
+mongoimport --db Thu19516531 --collection departments --file departments.json 
+mongoimport --db Thu19516531  --collection doctors --file doctors.json
+mongoimport --db Thu19516531  --collection patients --file patients.json  
+pause
