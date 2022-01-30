e2b07ef340ca4e0e9494bbf6e587dfc8796361cb
ANTi1302 <porkoe3443334@gmail.com>
2022-01-30 23:49:05 +0700

    demo

--- WebBanHangQuanAo/src/main/java/iuh/fit/controller/LoginController.java
+++ WebBanHangQuanAo/src/main/java/iuh/fit/controller/LoginController.java
@@ -2,33 +2,29 @@ package iuh.fit.controller;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import iuh.fit.until.HibernateUtil;
+public class LoginController extends HttpServlet{
 
-@WebServlet(urlPatterns = "/login")
-public class LoginController extends HttpServlet {
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=UTF-8");
-		String nameuser= req.getParameter("user");
-		String password=req.getParameter("password");
-	
-	}
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=UTF-8");
-		String nameuser= req.getParameter("user");
-		String password=req.getParameter("password");
-	
+		resp.setContentType("text/html");
+		String ten=req.getParameter("name");
+		String pass=req.getParameter("pass");
+
 	}
-	
-	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html");
+		String ten=req.getParameter("name");
+		String pass=req.getParameter("pass");
+	}
 }
